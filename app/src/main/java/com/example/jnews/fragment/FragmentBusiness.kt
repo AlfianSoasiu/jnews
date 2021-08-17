e776311cf4c126db2a54c89950b832e2da1f28d0
Alfian Sosaiu <alfiansoasiu@gmail.com>
2021-08-17 17:37:48 +0700

    1

---
+++ app/src/main/java/com/example/jnews/fragment/FragmentBusiness.kt
@@ -0,0 +1,84 @@
+package com.example.jnews.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.jnews.R
+import com.example.jnews.adapter.NewsAdapter
+import com.example.jnews.model.ModelArticle
+import com.example.jnews.model.ModelNews
+import com.example.jnews.networking.ApiEndpoint.getApiClient
+import com.example.jnews.networking.ApiInterface
+import com.example.jnews.util.Utils.getCountry
+import kotlinx.android.synthetic.main.fragment_news.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.util.*
+
+
+class FragmentBusiness : Fragment() {
+
+    companion object {
+        const val API_KEY = "43c3194110e042f9a09ed6d029ef5e67"
+    }
+
+    var strCategory = "business"
+    var strCountry: String? = null
+    var modelArticle: MutableList<ModelArticle> = ArrayList()
+    var newsAdapter: NewsAdapter? = null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_news, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        tvTitle.setText("Berita Bisnis")
+
+        rvListNews.setLayoutManager(LinearLayoutManager(context))
+        rvListNews.setHasFixedSize(true)
+        rvListNews.showShimmerAdapter()
+
+        //reload news
+        imageRefresh.setOnClickListener {
+            rvListNews.showShimmerAdapter()
+            getListNews()
+        }
+
+        //get news
+        getListNews()
+    }
+
+    //set api
+    private fun getListNews() {
+
+        //get country/
+        strCountry = getCountry()
+
+        //set api
+        val apiInterface = getApiClient().create(ApiInterface::class.java)
+        val call = apiInterface.getBusiness(strCountry, strCategory, API_KEY)
+        call.enqueue(object : Callback<ModelNews> {
+            override fun onResponse(call: Call<ModelNews>, response: Response<ModelNews>) {
+                if (response.isSuccessful && response.body() != null) {
+                    modelArticle = response.body()?.modelArticle as MutableList<ModelArticle>
+                    newsAdapter = NewsAdapter(modelArticle, context!!)
+                    rvListNews.adapter = newsAdapter
+                    newsAdapter?.notifyDataSetChanged()
+                    rvListNews.hideShimmerAdapter()
+                }
+            }
+
+            override fun onFailure(call: Call<ModelNews>, t: Throwable) {
+                Toast.makeText(context, "Oops, jaringan kamu bermasalah.", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+}
---
+++ app/src/main/java/com/example/jnews/model/ModelNews.kt
@@ -0,0 +1,15 @@
+package com.example.jnews.model
+
+import com.google.gson.annotations.SerializedName
+
+
+data class ModelNews(
+    @SerializedName("status")
+    val status: String = "",
+
+    @SerializedName("totalResults")
+    val totalResults: Int = 0,
+
+    @SerializedName("articles")
+    val modelArticle: List<ModelArticle> = emptyList()
+)
